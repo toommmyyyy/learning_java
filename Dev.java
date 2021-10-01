6d4adbc4fc2a392c4474cdabb08d5652dc1e01bf
Ankur Chaudhary <ac7664625@gmail.com>
2021-10-01 19:14:23 +0530

    addded feature multiply

--- Dev.java
+++ Dev.java
@@ -1,5 +1,9 @@
 public class Dev {
   public static void main(String[] args) {
+    int a=19;
+    int b=12;
+    int c=a*b;
+    System.out.println("multiply is :" +c);
     System.out.println("this is dev branch file");
   }
   
