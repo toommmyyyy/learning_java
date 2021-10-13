a8e2622efb3e5aed14fa32a2be109c0af9722a1a
Ankur Chaudhary <ac7664625@gmail.com>
2021-10-13 10:19:35 +0530

    palindrome_string.java file added

--- Check_palind.java
+++ Check_palind.java
@@ -1,6 +1,7 @@
 
 public class Check_palind {
-  String s1,s2,str;
+  String str;
+ 
   Check_palind(String str1) // constructor for 1 string
   {
      str=str1;
@@ -12,27 +13,26 @@ Check_palind(String str1,String str2) // constructor for 2 string
 
 // method to check if it is palindrome or not 
 boolean is_plaindrome()
-{
-  str=str.toLowerCase();
+{ String orginal=str; // to return original string in output
+  str=str.toLowerCase(); 
   for(int i=0;i<=str.length()/2;i++)
   {
-    char s= str.charAt(i); 
-     
-    char e=str.charAt(str.length()-1-i);
+    char s= str.charAt(i);   // pointing to first element
+    char e=str.charAt(str.length()-1-i); // pointing to last element 
     if(s!=e)
-    { System.out.println(str+" is not plaindrome.");
-      return false;
+    { System.out.println(orginal+" is not plaindrome.");
+      return false; 
     }
 
   }
-  System.out.println(str+"  is plaindrome.");
+  System.out.println(orginal+"  is plaindrome.");
   return true;
 }
 
 public static void main(String[] args) {
-  Check_palind S1=new Check_palind("Mom");
-  Check_palind S2=new Check_palind("Varnika","singh");
-  Check_palind S3=new Check_palind("123321");
+  Check_palind S1=new Check_palind("Mom"); // palindrome hai ye
+  Check_palind S2=new Check_palind("Varnika","singh");  // pailndrome nahi hai ye
+  Check_palind S3=new Check_palind("123321");  // palindrome hai ye
   S1.is_plaindrome();
   S2.is_plaindrome();
   S3.is_plaindrome();
